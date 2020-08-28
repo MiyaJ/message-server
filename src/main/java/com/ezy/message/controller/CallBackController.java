14aef92927d771652085bce51c3f8d4cde04b9a7
CaiXiaowei <xiaowei.cai@ezhiyang.com>
2020-08-28 11:23:23 +0800

    feat: 回调解析与配置

--- src/main/java/com/ezy/message/controller/CallBackController.java
+++ src/main/java/com/ezy/message/controller/CallBackController.java
@@ -12,10 +12,12 @@ import com.ezy.message.model.callback.contact.ExtAttr;
 import com.ezy.message.model.callback.contact.Item;
 import com.ezy.message.model.callback.contact.ItemText;
 import com.ezy.message.reception.Producer;
-import com.ezy.message.utils.crypto.WxCryptUtil;
+import com.ezy.message.utils.QywxCallBackUtils;
+import com.ezy.message.utils.aes.WXBizMsgCrypt;
 import com.thoughtworks.xstream.XStream;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,8 +38,15 @@ public class CallBackController {
     @Autowired
     private Producer producer;
 
-    private String token;
-    private String encodingAesKey;
+    @Value("${qywx.approval:token}")
+    private String APPROVAL_TOKEN;
+    @Value("${qywx.approval:encodingAesKey}")
+    private String APPROVAL_ENCODING_AES_KEY;
+    @Value("${qywx.contact:token}")
+    private String CONTACT_TOKEN;
+    @Value("${qywx.contact:encodingAesKey}")
+    private String CONTACT_ENCODING_AES_KEY;
+    @Value("${qywx.corpid}")
     private String corpid;
 
     /**
@@ -49,34 +58,43 @@ public class CallBackController {
      * @return 
      */
     @RequestMapping("/approval")
-    public void approval(HttpServletRequest request, HttpServletResponse response) {
-        // 加密签名
-        String msgSignature = request.getParameter("msg_signature");
-        String timestamp = request.getParameter("timestamp");
+    public String approval(HttpServletRequest request, HttpServletResponse response) {
+
+        String msgSig = request.getParameter("msg_signature");
+        String timeStamp = request.getParameter("timestamp");
         String nonce = request.getParameter("nonce");
-        // 加密的字符串。需要解密得到消息内容明文，解密后有random、msg_len、msg、receiveid四个字段，其中msg即为消息内容明文
         String echostr = request.getParameter("echostr");
-
+        log.info("msgSig:"+msgSig+"======timeStamp:"+timeStamp+"=====nonce:"+nonce+"=====echostr:"+echostr);
         try {
+            if ("get".equalsIgnoreCase(request.getMethod())){
+                WXBizMsgCrypt wxcpt = wxcpt(APPROVAL_TOKEN, APPROVAL_ENCODING_AES_KEY);
+                //需要返回的明文
+                String sEchoStr;
+                sEchoStr = wxcpt.VerifyURL(msgSig, timeStamp, nonce, echostr);
+                return sEchoStr;
 
-            WxCryptUtil wxCryptUtil = new WxCryptUtil(token, encodingAesKey, corpid);
-            String msgXmlStr = wxCryptUtil.decrypt(msgSignature, timestamp, nonce, echostr);
-            //必须要返回解密之后的明文
-            response.getWriter().write(msgXmlStr);
+            }else{
+                WXBizMsgCrypt wxcpt = wxcpt(APPROVAL_TOKEN, APPROVAL_ENCODING_AES_KEY);
+                String data = QywxCallBackUtils.getStringInputstream(request);
+                log.info("post请求数据: {}", data);
+                String content = wxcpt.DecryptMsg(msgSig,timeStamp,nonce,data);
+                log.info("解析后的消息: {}", content);
+                XStream xstream = new XStream();
+                //使用注解修改对象名称
+                xstream.processAnnotations(new Class[]{AppravalCallbackMessage.class, ApprovalInfo.class, ApprovalNode.class,
+                        ApprovalNodeItem.class, ApprovalNotifyNode.class});
+                //将字符串类型的xml转换为对象
 
-            log.info("msgXmlStr --->{}", msgXmlStr);
+                AppravalCallbackMessage callbackMessage = (AppravalCallbackMessage) xstream.fromXML(content);
 
-            XStream xstream = new XStream();
-            //使用注解修改对象名称
-            xstream.processAnnotations(new Class[]{AppravalCallbackMessage.class, ApprovalInfo.class, ApprovalNode.class,
-                    ApprovalNodeItem.class, ApprovalNotifyNode.class});
-            //将字符串类型的xml转换为对象
-            AppravalCallbackMessage callbackMessage = (AppravalCallbackMessage) xstream.fromXML(msgXmlStr);
-
-            // TODO: 2020/7/1 回调信息返回调用方: mq?
-            producer.send(RabbitQueueEnum.APPROVAL.getName(), JSONObject.toJSONString(callbackMessage));
+                // TODO: 2020/7/1 回调信息返回调用方: mq?
+                producer.send(RabbitQueueEnum.APPROVAL.getName(), JSONObject.toJSONString(callbackMessage));
+                return "success";
+            }
         } catch (Exception e) {
-
+            e.printStackTrace();
+            log.error("解析微信校验数据异常: {}", e.getMessage());
+            throw new RuntimeException("解析微信校验数据异常");
         }
 
     }
@@ -90,33 +108,53 @@ public class CallBackController {
      * @return
      */
     @RequestMapping("/contact")
-    public void contact(HttpServletRequest request, HttpServletResponse response) {
-        // 加密签名
-        String msgSignature = request.getParameter("msg_signature");
-        String timestamp = request.getParameter("timestamp");
+    public String contact(HttpServletRequest request, HttpServletResponse response) {
+
+        String msgSig = request.getParameter("msg_signature");
+        String timeStamp = request.getParameter("timestamp");
         String nonce = request.getParameter("nonce");
-        // 加密的字符串。需要解密得到消息内容明文，解密后有random、msg_len、msg、receiveid四个字段，其中msg即为消息内容明文
         String echostr = request.getParameter("echostr");
-
+        log.info("msgSig:"+msgSig+"======timeStamp:"+timeStamp+"=====nonce:"+nonce+"=====echostr:"+echostr);
         try {
+            if ("get".equalsIgnoreCase(request.getMethod())){
+                WXBizMsgCrypt wxcpt = wxcpt(CONTACT_TOKEN, CONTACT_ENCODING_AES_KEY);
+                //需要返回的明文
+                String sEchoStr;
+                sEchoStr = wxcpt.VerifyURL(msgSig, timeStamp, nonce, echostr);
+                return sEchoStr;
 
-            WxCryptUtil wxCryptUtil = new WxCryptUtil(token, encodingAesKey, corpid);
-            String msgXmlStr = wxCryptUtil.decrypt(msgSignature, timestamp, nonce, echostr);
-            //必须要返回解密之后的明文
-            response.getWriter().write(msgXmlStr);
+            }else{
+                WXBizMsgCrypt wxcpt = wxcpt(CONTACT_TOKEN, CONTACT_ENCODING_AES_KEY);
+                String data = QywxCallBackUtils.getStringInputstream(request);
+                log.info("post请求数据: {}", data);
+                String content = wxcpt.DecryptMsg(msgSig,timeStamp,nonce,data);
+                log.info("解析后的消息: {}", content);
 
-            log.info("msgXmlStr --->{}", msgXmlStr);
-
-            XStream xstream = new XStream();
-            xstream.processAnnotations(new Class[]{Contact.class, ExtAttr.class, Item.class, ItemText.class});
-            // 解析通讯类
-            Contact contact = (Contact) xstream.fromXML(msgXmlStr);
-
-            // TODO: 2020/7/1 回调信息返回调用方: mq?
-            producer.send(RabbitQueueEnum.CONTACT.getName(), JSONObject.toJSONString(contact));
+                XStream xstream = new XStream();
+                xstream.processAnnotations(new Class[]{Contact.class, ExtAttr.class, Item.class, ItemText.class});
+                // 解析通讯类
+                Contact contact = (Contact) xstream.fromXML(content);
+                // TODO: 2020/7/1 回调信息返回调用方: mq?
+                producer.send(RabbitQueueEnum.CONTACT.getName(), JSONObject.toJSONString(contact));
+                return "success";
+            }
         } catch (Exception e) {
-
+            e.printStackTrace();
+            log.error("解析微信校验数据异常: {}", e.getMessage());
+            throw new RuntimeException("解析微信校验数据异常");
         }
+        
+    }
 
+    private WXBizMsgCrypt wxcpt(String qywxToken, String encodingKey){
+        WXBizMsgCrypt wxcpt = null;
+        try {
+            wxcpt = new WXBizMsgCrypt(qywxToken, encodingKey, corpid);
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.error("解析微信校验数据异常: {}", e.getMessage());
+            throw new RuntimeException("企业微信校验异常");
+        }
+        return wxcpt;
     }
 }
